9a1d4dda1b3350cfd3262b1b1d226e32b158b253
abhaybarhate <abhaybarhate5@gmail.com>
2022-03-03 19:36:35 +0530

    added weapon switched script without func

---
+++ Assets/_Game/Scripts/Weapons/WeaponSwitcher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    private int currentWeaponIndex = 0;
+
+    void Start()
+    {
+
+    }
+
+    void SetActiveWeapon()
+    {
+
+    }
+
+    void ProcessWeaponKeyInput()
+    {
+
+    }
+
+    void ProcessWeaponScrollInput()
+    {
+
+    }
+
+}
